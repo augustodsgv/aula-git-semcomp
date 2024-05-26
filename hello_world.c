dfd40470358110b7f434354875d361e0f7a84046
augusto <augustodsgv@localhost.localdomain>
2024-05-26 11:26:44 -0300

    fix: name could be null, now treats this exception

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char * argv[]){
-	
+	if (argc < 2){
+		printf("falta o nome a ser printado!!!\n");
+		exit(1);
+	}
 	printf("Hello, %s!\n", argv[1]);
 
 
