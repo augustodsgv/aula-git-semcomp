ea7da1bc95655d30383261f09bd24c5d244f1e08
augusto <augustodsgv@localhost.localdomain>
2024-05-26 11:31:59 -0300

    mudando a mensagem de erro

--- hello_world.c
+++ hello_world.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char * argv[]){
 	if (argc < 2){
-		printf("falta o nome a ser printado!!!\n");
+		printf("O nome a ser printado deve ser passado!!!\n");
 		exit(1);
 	}
 	printf("Hello, %s!\n", argv[1]);
